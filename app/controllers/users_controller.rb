5e9344bb684f67e7dc3be28574c94d95cede9ed3
annymesc <anny.escalante@gmail.com>
2016-06-10 13:14:19 -0400

    hywvdbw

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = "User Created."
       redirect_to root_url
     else
       render 'new'
